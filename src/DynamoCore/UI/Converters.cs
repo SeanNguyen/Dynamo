1f972baf6734e9c8ea274017b87567a1d0201431
Sean-Nguyen <firestormkjc.1993@gmail.com>
2013-10-11 10:50:48 +0800

    Updated Visibility of resize bars

--- src/DynamoCore/UI/Converters.cs
+++ src/DynamoCore/UI/Converters.cs
@@ -1067,4 +1067,19 @@ namespace Dynamo.Controls
         }
     }
 
+    public class InfoBubbleStyleToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((InfoBubbleViewModel.Style) value == InfoBubbleViewModel.Style.Preview)
+                return Visibility.Visible;
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
 }
